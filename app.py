99143f0d8fcfe5f46f16ce331118d4ddfbb5deee
dtmurphy6 <102379855+dtmurphy6@users.noreply.github.com>
2022-08-17 21:16:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_costa
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/macro_db")
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
