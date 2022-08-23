1014468d400ce55b9faa612df40b75fcb42c6450
tbryan2 <timbryan0315@gmail.com>
2022-08-22 21:27:40 -0500

    A few more JS edits...

--- static/plots.js
+++ static/plots.js
@@ -1,36 +1,38 @@
+// Define the map variable
 var myMap = L.map("map", {
     center: [39, 98],
     zoom: 5
 });
 
 // Adding a tile layer (the background map image) to our map:
-// We use the addTo() method to add objects to our map.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-
+// Define the unemployment API endpoint URL
 var unemployment_url = "http://127.0.0.1:5000/api/unemployment";
 
+// Define the initalization function
+function init() {
+    d3.json(unemployment_url).then(function (response) {
 
+        //console.log(response);
 
-d3.json(unemployment_url).then(function (response) {
+        var heatArray = [];
 
-    console.log(response);
-55
-    // var heatArray = [];
+        for (var i = 0; i < response.data; i++) {
 
-    // for (var i = 0; i < response.length; i++) {
-    //     var location = response[i].location;
+            var year = response[i].data;
 
-    //     if (location) {
-    //         heatArray.push([location.coordinates[1], location.coordinates[0]]);
-    //     }
-    // }
+    //         if (location) {
+    //             heatArray.push([location.coordinates[1], location.coordinates[0]]);
+    //         }
+        }
 
     // var heat = L.heatLayer(heatArray, {
     //     radius: 20,
     //     blur: 35
     // }).addTo(myMap);
+}
 
-});
+init();
