e419a853ace024dc182aec224bb7be060836f014
dtmurphy6 <102379855+dtmurphy6@users.noreply.github.com>
2022-08-22 21:06:54 -0500

    outputting data to our own API

--- app.py
+++ app.py
@@ -1,23 +1,64 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_costa
+from flask import Flask, render_template, redirect, jsonify
+import pymongo
+import json
+from bson.json_util import dumps
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/macro_db")
-
-
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    data = mongo.db.collection.find_one()
+def home():    
 
     # Return template and data
-    return render_template("index.html", data)
+    return render_template("index.html")
+
+@app.route("/api/unemployment")
+def unemployment():
+    
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+
+    # Declare the database
+    db = client.macro_db
+
+    # Declare the unemployment rate collection
+    unemployment = db.unemployment
+    unemployment_data = dumps(list(unemployment.find()))
+
+    return jsonify(unemployment_data)
+
+@app.route("/api/percapincome")
+def percapincome():
+    # Build the MongoDB database and collections
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+
+    # Declare the database
+    db = client.macro_db
+
+    # Declare the per capita income collection
+    percapincome = db.percapincome
+    percapincome_data = dumps(list(percapincome.find()))
+
+    # Return template and data
+    return jsonify(percapincome_data)
+
+@app.route("/api/minimum_wage")
+def minimum_wage():
+     # Build the MongoDB database and collections
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+
+    # Declare the database
+    db = client.macro_db
+
+    # Declare the minimum wage collection
+    minimum_wage = db.minimum_wage
+    minimum_wage_data = dumps(list(minimum_wage.find()))
+
+    # Return template and data
+    return jsonify(minimum_wage_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
