4b247e72f177eccf3f2729bbb7eca3fbdf9fc10e
tbryan2 <timbryan0315@gmail.com>
2022-08-22 21:06:04 -0500

    Flask edits

--- app.py
+++ app.py
@@ -1,75 +1,67 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+from flask import jsonify
+import json
+from bson.json_util import dumps
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/macro_db")
-
-# Build the MongoDB database and collections
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-
-# Declare the database
-db = client.macro_db
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/macro_db"
+client = PyMongo(app)
 
 # Declare the unemployment rate collection
-unemployment = db.unemployment
-
-# Collecting unemployment data
-for date in db.unemployment
-
-myquery = { "id": { "$gt": "S" } }
-
+unemployment = client.db.unemployment
 
 # Declare the per capita income collection
-percapincome = db.percapincome
+percapincome = client.db.percapincome
 
 # Declare the minimum wage collection
-minimum_wage = db.minimum_wage
-# Route to render index.html template using data from Mongo
+minimum_wage = client.db.minimum_wage
+
+
 @app.route("/")
 def home():
 
-    # Find one record of data from the mongo database
-    data = mongo.db.collection.find_one()
-    
-    #for loop for each collection
-
     # Return template and data
-    return render_template("index.html", data)
+    return render_template("index.html")
 
-@app.route("api/unemployment")
+@app.route("/api/unemployment")
 def unemployment():
 
+    docs = []
     # Find one record of data from the mongo database
-    data = mongo.db.unemployment.find()
+    for doc in client.db.unemployment.find():
+        docs.append(doc['meta'])
+
+    return jsonify(docs)
     
-##### MENTAL NOTE: RETURN HERE AFTER JAVA
 
-    return jsonify(data)
-
-@app.route("api/percapincome")
+@app.route("/api/percapincome")
 def percapincome():
 
+    docs = []
     # Find one record of data from the mongo database
-    data = mongo.db.percapincome.find()
-    
-    #for loop for each collection
-    # Return template and data
-    return jsonify(data)
+    for doc in client.db.percapincome.find():
+        docs.append(doc['meta'])
 
-@app.route("api/minimum_wage")
+    return jsonify(docs)
+
+@app.route("/api/minimum_wage")
 def minimum_wage():
 
+    docs = []
     # Find one record of data from the mongo database
-    data = mongo.db.minimum_wage.find()
-    
-    #for loop for each collection
-    # Return template and data
-    return jsonify(data)
+    for doc in client.db.minimum_wage.find():
+        docs.append(doc['meta'])
+
+    return jsonify(docs)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
--- static/plots.js
+++ static/plots.js
@@ -1,6 +1,6 @@
 var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
+    center: [39, 98],
+    zoom: 5
 });
 
 // Adding a tile layer (the background map image) to our map:
@@ -10,5 +10,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
 
 
-var uneployment_url = "http://127.0.0.1:5500/api/unemployment";
+var unemployment_url = "http://127.0.0.1:5000/api/unemployment";
 
+
+
+d3.json(unemployment_url).then(function (response) {
+
+    console.log(response);
+55
+    // var heatArray = [];
+
+    // for (var i = 0; i < response.length; i++) {
+    //     var location = response[i].location;
+
+    //     if (location) {
+    //         heatArray.push([location.coordinates[1], location.coordinates[0]]);
+    //     }
+    // }
+
+    // var heat = L.heatLayer(heatArray, {
+    //     radius: 20,
+    //     blur: 35
+    // }).addTo(myMap);
+
+});
